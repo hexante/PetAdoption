5f1ccb88927b098e07fc22cffe5ba5fc9dc2fb70
Harby <Harbymina8@gmail.com>
2019-03-28 00:38:53 -0500

    formulario registro mascotas

---
+++ app/src/main/java/com/example/petadoption/AccoutActivity/DatosMascotas.java
@@ -0,0 +1,15 @@
+package com.example.petadoption.AccoutActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.petadoption.R;
+
+public class DatosMascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_mascotas);
+    }
+}
